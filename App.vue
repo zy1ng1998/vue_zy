418599c38f5285d218a278262bf9b2128d97fdfc
zy1ng1998 <1905202023@qq.com>
2020-05-24 23:38:56 +0800

    33

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        姓名{{ name }}
+        <p>父元素P</p>
+        <base-son />
+    </div>
+</template>
+
+<script>
+import baseSon from "./Son"
+export default {
+    components:{
+        baseSon
+    },
+    data(){
+        return{
+            name:'zy'
+        }
+    }
+}
+</script>
+
+<style>
+div{
+    background-color: blueviolet;
+}
+</style>
---
+++ Son.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        son
+<p> 这是一个P标签</p>
+    </div>
+</template>
+
+<style scoped>
+p{
+    background-color: chartreuse;
+}
+</style>
