0401bea7c2f6c00dd7db781d5d8f4d30ab768f5b
zy1ng1998 <1905202023@qq.com>
2020-05-28 14:37:21 +0800

    37

---
+++ zy-vue/zy-vue/src/components/BaseLevel.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    functional:true,
+    props:{
+        level:{
+            type:Number,
+            required:true,
+        }
+    },
+
+    render(h,context){
+        console.log(context)
+        const { props , slots} = context;
+        console.log(props);
+        console.log(slots());
+        const tag ='h' + props.level
+        return(
+            <tag>
+            { slots().default }
+            </tag>
+        )
+    }
+}
+</script>
